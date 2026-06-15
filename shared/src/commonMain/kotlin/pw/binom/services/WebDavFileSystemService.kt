c344effec6ff720ebeb1f2fd5b7577a72719aa89
subochev <caffeine.mgn@gmail.com>
2026-06-15 03:37:23 +0300

    Delegate WebDavFileSystemService operations to MountedFileSystem implementation.

--- shared/src/commonMain/kotlin/pw/binom/services/WebDavFileSystemService.kt
+++ shared/src/commonMain/kotlin/pw/binom/services/WebDavFileSystemService.kt
@@ -4,51 +4,42 @@ import kotlinx.io.files.Path
 import pw.binom.webdav.fs.CopyOrMoveResult
 import pw.binom.webdav.fs.FileMetadata
 import pw.binom.webdav.fs.WebDavFileSystem
+import pw.binom.webdav.fs.mounted.MountedFileSystem
 
-class WebDavFileSystemService: WebDavFileSystem {
+class WebDavFileSystemService : WebDavFileSystem {
 
-    override suspend fun list(path: Path): Result<List<FileMetadata>> {
-        TODO("Not yet implemented")
-    }
+    private val mountedFileSystem = MountedFileSystem()
 
-    override suspend fun getMetadata(path: Path): Result<FileMetadata> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun list(path: Path): Result<List<FileMetadata>> =
+        mountedFileSystem.list(path)
+
+    override suspend fun getMetadata(path: Path): Result<FileMetadata> =
+        mountedFileSystem.getMetadata(path)
 
     override suspend fun readFile(
         path: Path,
         range: LongRange?
-    ): Result<ByteArray> {
-        TODO("Not yet implemented")
-    }
+    ): Result<ByteArray> = mountedFileSystem.readFile(path, range)
 
     override suspend fun writeFile(
         path: Path,
         content: ByteArray,
         overwrite: Boolean
-    ): Result<Unit> {
-        TODO("Not yet implemented")
-    }
+    ): Result<Unit> = mountedFileSystem.writeFile(path = path, content = content, overwrite = overwrite)
 
-    override suspend fun createDirectory(path: Path): Result<Unit> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun createDirectory(path: Path): Result<Unit> =
+        mountedFileSystem.createDirectory(path)
 
-    override suspend fun delete(path: Path): Result<Unit> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun delete(path: Path): Result<Unit> =
+        mountedFileSystem.delete(path)
 
     override suspend fun move(
         source: Path,
         destination: Path
-    ): Result<CopyOrMoveResult> {
-        TODO("Not yet implemented")
-    }
+    ): Result<CopyOrMoveResult> = mountedFileSystem.move(source = source, destination = destination)
 
     override suspend fun copy(
         source: Path,
         destination: Path
-    ): Result<CopyOrMoveResult> {
-        TODO("Not yet implemented")
-    }
+    ): Result<CopyOrMoveResult> = mountedFileSystem.copy(source = source, destination = destination)
 }
